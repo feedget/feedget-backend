db50c2502645073b28d548736bf4178500e21d36
Dong Hee Kim <opklnm102@gmail.com>
2017-12-24 17:49:53 +0900

    #29 Add: H2 connection init 추가

--- feedget-api/src/test/java/kr/co/mashup/feedgetapi/FeedgetApiApplicationTests.java
+++ feedget-api/src/test/java/kr/co/mashup/feedgetapi/FeedgetApiApplicationTests.java
@@ -3,10 +3,12 @@ package kr.co.mashup.feedgetapi;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@ActiveProfiles(profiles = "test")
 public class FeedgetApiApplicationTests {
 
 	@Test
