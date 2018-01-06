1333ced035a3b22284fadeb23adeef0107781c10
Dong Hee Kim <opklnm102@gmail.com>
2018-01-07 04:29:23 +0900

    #47 Add: 창작물 컨텐츠 삭제 API 구현

--- feedget-api/src/test/java/kr/co/mashup/feedgetapi/web/controller/ContentsControllerTest.java
+++ feedget-api/src/test/java/kr/co/mashup/feedgetapi/web/controller/ContentsControllerTest.java
@@ -2,7 +2,6 @@ package kr.co.mashup.feedgetapi.web.controller;
 
 import kr.co.mashup.feedgetapi.service.ContentsService;
 import kr.co.mashup.feedgetapi.web.dto.ContentsDto;
-import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -14,9 +13,13 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
+import java.util.Arrays;
+import java.util.List;
+
 import static org.mockito.Matchers.any;
 import static org.mockito.Matchers.eq;
 import static org.mockito.Mockito.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.fileUpload;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -100,5 +103,37 @@ public class ContentsControllerTest {
 
         // then : 컨텐츠가 추가되지 않는다
         verify(contentsService, never()).addContents(eq(creationId), any(ContentsDto.class));
+    }
+
+    @Test
+    public void deleteContents_창작물의_컨텐츠_제거_성공() throws Exception {
+        // given : 창작물 ID, 제거할 컨텐츠 ID 리스트로
+        long creationId = 1L;
+        List<Long> contentIds = Arrays.asList(1L, 2L, 3L);
+
+        // when : 창작물의 컨텐츠를 제거하면
+        MvcResult result = mockMvc.perform(delete("/creations/{creationId}/contents", creationId)
+                .param("contentId", String.valueOf(1L))
+                .param("contentId", String.valueOf(2L))
+                .param("contentId", String.valueOf(3L))
+        ).andExpect(status().isOk())
+                .andReturn();
+
+        // then : 컨텐츠가 제거된다
+        verify(contentsService, times(1)).removeContents(creationId, contentIds);
+    }
+
+    @Test
+    public void deleteContents_창작물의_컨텐츠_제거_제거할_컨텐츠_ID가_없어서_실패() throws Exception {
+        // given : 창작물 ID
+        long creationId = 1L;
+
+        // when : 창작물의 컨텐츠를 제거하면
+        MvcResult result = mockMvc.perform(delete("/creations/{creationId}/contents", creationId)
+        ).andExpect(status().isBadRequest())
+                .andReturn();
+
+        // then : 컨텐츠가 제거되지 않는다
+        verify(contentsService, never()).removeContents(eq(creationId), anyListOf(Long.class));
     }
 }
